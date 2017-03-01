aa2c7afa3b55662919fdeaeab4fd7266cf575064
SeeForward <likeblade@sina.com>
2017-03-01 20:17:13 +0800

    add memeory status

---
+++ src/include/monitor/memory_state.h
@@ -0,0 +1,35 @@
+﻿//Date:2016-3-4
+//Author:lidengke
+//Desc:query the information and state about physical memory and virtual memory
+
+#ifndef __MEMORY_STATE_H__
+#define __MEMORY_STATE_H__
+
+#include "type_def.h"
+#include "memory_info.h"
+
+
+class MemoryState 
+{
+public:
+	MemoryState() 
+	{
+		m_usage = 0.0;
+		m_totalPhys = 0;
+		m_availPhys = 0;
+		m_totalVirt = 0;
+		m_availVirt = 0;
+	}
+
+	//get memory's state
+	bool GetState();
+
+public:
+	float 	m_usage;	//usage rate(%)
+	uint64_t m_totalPhys;	//total physical size (byte)
+	uint64_t m_availPhys;	//availible physical size (byte)
+	uint64_t m_totalVirt;	//total virtual size (byte)
+	uint64_t m_availVirt;	//availible virtual size (byte)
+};
+
+#endif //__MEMORY_STATE_H__
---
+++ src/lib/monitor/memory_state.cpp
@@ -0,0 +1,90 @@
+﻿//Date:2016-3-7
+//Author:lidengke
+
+#include "memory_state.h"
+#include "system_tool.h"
+
+#ifdef __WINDOWS__
+#	include <Windows.h>
+#	include <TlHelp32.h>
+#	include <Psapi.h>
+#	include <comdef.h>
+#	include <WbemIdl.h>
+#	include <sstream>
+
+	using std::stringstream;
+#else
+#	include <unistd.h>
+#	include <stdio.h>
+#	include <string.h>
+#	include <dirent.h>
+#	include <stdlib.h>
+#endif //__WINDOWS__
+
+#include <string>
+
+using std::string;
+
+bool MemoryState::GetState() 
+{
+#ifdef __WINDOWS__
+	MEMORYSTATUSEX mse;
+	mse.dwLength = sizeof(mse);
+	if(!::GlobalMemoryStatusEx(&mse)) {
+		return false;
+	}
+
+	m_usage = (float)mse.dwMemoryLoad;
+	m_totalPhys = mse.ullTotalPhys;
+	m_availPhys = mse.ullAvailPhys;
+	m_totalVirt = mse.ullTotalVirtual;
+	m_availVirt = mse.ullAvailVirtual;
+#else
+	FILE* fp = fopen("/proc/meminfo", "r");
+	if (!fp) {
+		return false;
+	}
+
+	char buf[256];
+	int getItemNum = 0;
+	uint64_t buffers = 0;
+	uint64_t cached = 0;
+	while (fgets(buf, sizeof(buf), fp))
+	{
+		if(strncmp(buf, "MemTotal:", 9) == 0) {
+			sscanf(buf, "%*s %"PRIu64, &m_totalPhys);
+			m_totalPhys *= 1024;
+			++getItemNum;
+		} else if(strncmp(buf, "MemFree:", 8) == 0) {
+			sscanf(buf, "%*s %"PRIu64, &m_availPhys);
+			m_availPhys *= 1024;
+			++getItemNum;
+		} else if(strncmp(buf, "Buffers:", 8) == 0) {
+			sscanf(buf, "%*s %"PRIu64, &buffers);
+			m_availPhys += buffers * 1024;
+		} else if(strncmp(buf, "Cached:", 7) == 0) {
+			sscanf(buf, "%*s %"PRIu64, &cached);
+			m_availPhys += cached * 1024;
+		} else if(strncmp(buf, "VmallocTotal:", 13) == 0) {
+			sscanf(buf, "%*s %"PRIu64, &m_totalVirt);
+			m_totalVirt *= 1024;
+			++getItemNum;
+		} else if(strncmp(buf, "VmallocUsed:", 12) == 0) {
+			uint64_t usedVirt = 0;
+			sscanf(buf, "%*s %"PRIu64, &usedVirt);
+			usedVirt *= 1024;
+			m_availVirt = m_totalVirt - usedVirt;
+			++getItemNum;
+		}
+	}
+	fclose(fp);
+
+	if (getItemNum < 4) {
+		return false;
+	}
+	m_usage = 100 * (m_totalPhys - m_availPhys) / (double)m_totalPhys;
+#endif //__WINDOWS__
+	return true;
+}
+
+
