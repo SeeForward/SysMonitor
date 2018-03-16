17fb58e905ee674e7cb51d10b9c784333276e749
SeeForward <likeblade@sina.com>
2018-03-16 21:22:22 +0800

    add function ToStr for vector convert to string

--- src/include/string_util.h
+++ src/include/string_util.h
@@ -1,10 +1,32 @@
 #ifndef __STRING_UTIL_H__
 #define __STRING_UTIL_H__
 
+#include "type_def.h"
+#include <string>
+#include <vector>
+#include <sstream>
 
 //checks if all chars of a string are digital chars
 bool IsNumber(const char* cp);
 
+// vector convert to string
+template<typename T>
+std::string ToStr(const std::vector<T> &items, char separator = ',')
+{
+	if (items.empty())
+	{
+		return "";
+	}
 
+	std::stringstream ss;
+	for (size_t i = 0; i < items.size() - 1; ++i)
+	{
+		ss << items[i] << separator;
+	}
+
+	ss << items[items.size() - 1];
+
+	return ss.str();
+}
 
 #endif //__STRING_UTIL_H__
