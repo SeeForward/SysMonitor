ea5a36729835a6e3f1a9f8fc8b2cf0189841e337
SeeForward <likeblade@sina.com>
2018-05-05 21:42:44 +0800

    ~SmartPtr delete m_ptr when DecInf

--- src/include/common/SmartPtr.h
+++ src/include/common/SmartPtr.h
@@ -9,96 +9,104 @@ template <typename T>
 class SmartPtr : public RefObj
 {
 public:
-	typedef T  ElementType;
-	typedef T* PointerType;
+    typedef T  ElementType;
+    typedef T* PointerType;
 
-	SmartPtr() : m_ptr(NULL) {}
+    SmartPtr() : m_ptr(NULL) {}
 
-	explicit SmartPtr(T* p) : m_ptr(p) {}
+    explicit SmartPtr(T* p) : m_ptr(p) {}
 
-	SmartPtr(const SmartPtr<T>& other) : RefObj(other), m_ptr(other.m_ptr) {}
+    SmartPtr(const SmartPtr<T>& other) : RefObj(other), m_ptr(other.m_ptr) {}
 
-	template <typename T1>
-	SmartPtr(const SmartPtr<T1>& other) : RefObj(other), m_ptr(dynamic_cast<T>(other.m_ptr)) {}
+    template <typename T1>
+    SmartPtr(const SmartPtr<T1>& other) : RefObj(other), m_ptr(dynamic_cast<T>(other.m_ptr)) {}
 
-	virtual ~SmartPtr() {}
+    virtual ~SmartPtr()
+    {
+        DecRef();
+        if (0 == Value())
+        {
+            delete m_ptr;
+            m_ptr = NULL;
+        }
+    }
 
-	bool isNull() const
-	{
-		return NULL == m_ptr;
-	}
+    bool isNull() const
+    {
+        return NULL == m_ptr;
+    }
 
-	operator bool() const
-	{
-		return NULL != m_ptr;
-	}
+    operator bool() const
+    {
+        return NULL != m_ptr;
+    }
 
-	bool operator!() const
-	{
-		return NULL == m_ptr;
-	}
+    bool operator!() const
+    {
+        return NULL == m_ptr;
+    }
 
-	T* Get() const
-	{
-		return m_ptr;
-	}
+    T* Get() const
+    {
+        return m_ptr;
+    }
 
-	T* operator->() const
-	{
-		return m_ptr;
-	}
+    T* operator->() const
+    {
+        return m_ptr;
+    }
 
-	T& operator*() const
-	{
-		return *m_ptr;
-	}
+    T& operator*() const
+    {
+        return *m_ptr;
+    }
 
-	bool operator==(const SmartPtr<T>& other) const
-	{
-		return m_ptr == other.m_ptr;
-	}
+    bool operator==(const SmartPtr<T>& other) const
+    {
+        return m_ptr == other.m_ptr;
+    }
 
-	bool operator!=(const SmartPtr<T>& other) const
-	{
-		return m_ptr != other.m_ptr;
-	}
+    bool operator!=(const SmartPtr<T>& other) const
+    {
+        return m_ptr != other.m_ptr;
+    }
 
 protected:
-	void Swap(SmartPtr<T>& other)
-	{
-		std::swap(m_ptr, other.m_ptr);
-		RefObj::Swap(other);
-	}
+    void Swap(SmartPtr<T>& other)
+    {
+        std::swap(m_ptr, other.m_ptr);
+        RefObj::Swap(other);
+    }
 
 private:
-	template <class T1>
-	friend class SmartPtr;
+    template <class T1>
+    friend class SmartPtr;
 
-	T*  m_ptr;
+    T*  m_ptr;
 };
 
 template <class T,class U>
 bool operator==(const SmartPtr<T>& lhs, const U* rhs)
 {
-	return lhs.Get() == rhs;
+    return lhs.Get() == rhs;
 }
 
 template <class T,class U>
 bool operator==(const U* lhs, const SmartPtr<T>& rhs)
 {
-	return lhs == rhs.Get();
+    return lhs == rhs.Get();
 }
 
 template <class T,class U>
 bool operator!=(const SmartPtr<T>& lhs, const U* rhs)
 {
-	return lhs.Get() != rhs;
+    return lhs.Get() != rhs;
 }
 
 template <class T,class U>
 bool operator!=(const U* lhs, const SmartPtr<T>& rhs)
 {
-	return lhs != rhs.Get();
+    return lhs != rhs.Get();
 }
 
 #endif //__SMART_PTR_H__
